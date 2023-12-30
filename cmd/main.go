4cab90eeb3be7ad06e80dcfb2af1fbb9a27b7762
luseid <lucas.seidman@mercadolibre.com>
2023-12-30 13:27:34 -0300

    prueba

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"modulo/pack"
+)
+
+
+
+
+func main() {
+
+fmt.Println(pack.Temperatura ,pack.Presion, pack.Humedad)
+
+}
---
+++ pack/info.go
@@ -0,0 +1,5 @@
+package pack
+
+var Temperatura int = 191111
+var Presion float64 = 200
+var Humedad string = "Seco"
